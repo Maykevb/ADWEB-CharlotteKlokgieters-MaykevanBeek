2764c0c192dd07ec4fd9aa8cb87bec870251a892
Mayke <mtmbeek@avans.nl>
2024-06-10 10:00:11 +0200

    search

--- ADWEB2324-CharlotteKlokgieters-MaykevanBeek/src/app/huishoudboekje-zoek.pipe.ts
+++ ADWEB2324-CharlotteKlokgieters-MaykevanBeek/src/app/huishoudboekje-zoek.pipe.ts
@@ -5,17 +5,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class HuishoudboekjeZoekPipe implements PipeTransform {
 
-  transform(value: any, query: string): any {
+  transform(value: any[], query: string): any[] {
     if (!value || !query) return value;
 
     query = query.toLocaleLowerCase();
 
     return value.filter((item: any) => {
-      let name = item.name.toLocaleLowerCase();
-
-      if (name.includes(query)) {
-        return item;
-      }
-    })
+      let naam = item.naam.toLocaleLowerCase();
+      return naam.includes(query);
+    });
   }
 }
