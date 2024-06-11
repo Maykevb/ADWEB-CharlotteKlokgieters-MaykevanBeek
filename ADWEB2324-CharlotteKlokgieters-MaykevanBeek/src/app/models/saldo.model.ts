dab2f897180251434e35c37401f7526e6ff7d7e1
Mayke <mtmbeek@avans.nl>
2024-06-11 10:02:18 +0200

    add-saldo

--- ADWEB2324-CharlotteKlokgieters-MaykevanBeek/src/app/models/saldo.model.ts
+++ ADWEB2324-CharlotteKlokgieters-MaykevanBeek/src/app/models/saldo.model.ts
@@ -3,14 +3,14 @@ import {Categorie} from "./categorie.model";
 export class Saldo {
   id: string;
   tag: string;
-  categorie: Categorie;
+  categorie: Categorie | null;
   bedrag: number;
 
-  constructor(id: string, tag: string, categorie: Categorie, bedrag: string) {
+  constructor(id: string, tag: string, bedrag: string) {
     this.id = id;
     this.tag = tag;
-    this.categorie = categorie;
     this.bedrag = parseFloat(bedrag);
+    this.categorie = null;
   }
 }
 
