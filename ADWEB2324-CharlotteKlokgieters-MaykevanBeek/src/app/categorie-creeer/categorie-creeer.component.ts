472c2537091913ef5163b6b7614dedcfcf477d8d
Charlotte Klokgieters <charlotteklokgieters@gmail.com>
2024-06-11 14:06:33 +0200

    categorie budget en datum

--- ADWEB2324-CharlotteKlokgieters-MaykevanBeek/src/app/categorie-creeer/categorie-creeer.component.ts
+++ ADWEB2324-CharlotteKlokgieters-MaykevanBeek/src/app/categorie-creeer/categorie-creeer.component.ts
@@ -10,14 +10,19 @@ import {CategorieService} from "../categorie.service";
 export class CategorieCreeerComponent {
   categorie: Categorie = new Categorie("", "");
 
-  constructor(private service: CategorieService) {
-
-  }
+  constructor(private service: CategorieService) { }
 
   onAdd() {
-    if (this.categorie.naam != "") {
+    if (this.categorie.eindDatum === undefined) {
+      this.categorie.eindDatum = null;
+    }
+    else {
+      console.log(this.categorie.eindDatum)
+    }
+    if (this.categorie.naam !== "" && this.categorie.budget != 0 && this.categorie.budget != null) {
       this.service.addCategorie(this.categorie);
       this.categorie = new Categorie("", "");
     }
   }
+
 }
--- ADWEB2324-CharlotteKlokgieters-MaykevanBeek/src/app/models/categorie.model.ts
+++ ADWEB2324-CharlotteKlokgieters-MaykevanBeek/src/app/models/categorie.model.ts
@@ -1,10 +1,16 @@
 export class Categorie {
   id: string;
   naam: string;
+  budget?: number;
+  eindDatum?: Date | null;
   editMode?: boolean;
 
-  constructor(id: string, naam: string) {
+  constructor(id: string, naam: string, eindDatum?: Date) {
     this.id = id;
     this.naam = naam;
+    // @ts-ignore
+    this.budget = null;
+    this.eindDatum = eindDatum;
+    this.editMode = false;
   }
 }
