3e724d56585521d11dee869fc1ac16544a08b5be
Charlotte Klokgieters <charlotteklokgieters@gmail.com>
2024-06-14 11:07:03 +0200

    hosting

--- ADWEB2324-CharlotteKlokgieters-MaykevanBeek/src/app/app.module.ts
+++ ADWEB2324-CharlotteKlokgieters-MaykevanBeek/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { environment } from '../environments/environment';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
